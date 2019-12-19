abe0ffe61914b6ee813aa6e72d2a747c4d2e6508
David Gomez <davdg25@gmail.com>
2019-12-19 09:25:36 -0700

    Create model testing for machine_snacks and snacks

---
+++ spec/models/machine_snacks_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe MachineSnack, type: :model do
+
+  describe 'validations' do
+    it {should belong_to :machine}
+    it {should belong_to :snack}
+  end
+
+end
