ee9e6d80be4f2c88322fd63f7fb2e5c8ff9c091f
David Gomez <davdg25@gmail.com>
2019-12-19 09:17:02 -0700

    Create feature test for vending machine show page

---
+++ spec/features/vending_machine/show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'As A Visitor' do
+  before(:each) do
+    @foxy = Owner.create!(name: 'Foxy')
+
+    @turing_machine = @foxy.machines.create!(location: 'Turing')
+
+    @candy = @turing_machine.snacks.create!(name: 'candy', price: 1.50)
+    @chips = @turing_mahcing.snacks.create!(name: 'chips', price: 1.00)
+  end
+
+  it 'shows each snack name and price associated with the vending machine' do
+    visit "/vending_machine/#{@turing_machine.id}"
+
+    expect(page).to have_content(@candy.name)
+    expect(page).to have_content(@candy.price)
+    expect(page).to have_content(@chips.name)
+    expect(page).to have_content(@chips.price)
+  end
+end
+
+
+#
+# User Story 1 of 3
+#
+# As a visitor
+# When I visit a vending machine show page
+# I see the name of all of the snacks associated with that vending machine along with their price
